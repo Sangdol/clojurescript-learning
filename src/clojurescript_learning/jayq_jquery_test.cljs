87c09bbc66cc27ae527b276153be3adf5ad60bb7
Sanghyun Lee <hammerha@gmail.com>
2020-05-08 22:45:45 +0200

    Change :use to :require

--- src/clojurescript_learning/jayq_jquery_test.cljs
+++ src/clojurescript_learning/jayq_jquery_test.cljs
@@ -2,25 +2,24 @@
   jayq https://github.com/ibdknox/jayq
 ")
 (ns clojurescript-learning.jayq-jquery-test
-  (:use [jayq.core :only [$ on text prop trigger]]))
+  (:require [jayq.core :as j]))
 
 (println "Hello Jayq!")
 
 #_("
-
   1. Bind a click event to an anchor tag.
   2. Get the id of the tag and set it inside a div.
 ")
 
-(def $click-me ($ :#click-me))
+(def $click-me (j/$ :#click-me))
 
 (defn on-click-me []
   (do
     (this-as this
-      (text ($ :.click-result) (->
-                                 ($ this)
-                                 (prop :id)))
+      (j/text (j/$ :.click-result) (->
+                                     (j/$ this)
+                                     (j/prop :id)))
       nil)))
 
-(on $click-me :click on-click-me)
-(trigger $click-me :click)
+(j/on $click-me :click on-click-me)
+(j/trigger $click-me :click)
