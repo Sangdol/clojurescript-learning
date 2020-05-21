b0b9779a36cff49742f9838093b610389fef74d4
Sanghyun Lee <hammerha@gmail.com>
2020-05-21 20:36:34 +0200

    Randomize color

--- src/nature_of_code/intro_3_gaussian_distribution.cljs
+++ src/nature_of_code/intro_3_gaussian_distribution.cljs
@@ -6,25 +6,38 @@
   (:require [quil.core :as q :include-macros true]
             [quil.middleware :as m]))
 
+(def FRAME_COUNT 3000)
+
 (defn setup []
   (q/background 240)
-  (q/frame-rate 3))
-  ;(q/stroke 0)
-  ;(q/stroke-weight 10)
-  ;(q/point 100 50))
+  (q/color-mode :hsl 360 100 100 100)
+  (q/frame-rate 60)
+  {:current-frame 0})
 
-(defn update [])
+(defn update [state]
+  (if (= (:current-frame state) FRAME_COUNT)
+    (do
+      (js/console.log "Finished")
+      (q/no-loop)))
+  (update-in state [:current-frame] inc))
+
+(defn rg [sd mean]
+  "random gaussian"
+  (+ (* (q/random-gaussian) sd) mean))
 
 (defn draw []
-  (q/stroke 0 10)
-  (q/stroke-weight 10)
-  (let [r (q/random-gaussian)
-        sd 30
-        mean (/ (q/width) 2)]
-    (q/point (+ (* r sd) mean) (/ (q/height) 2))))
+  ;; https://tylerxhobbs.com/essays/2016/working-with-color-in-generative-art
+  ;; http://colorizer.org/
+  (q/stroke-weight 3)
+  (let [x_mean (/ (q/width) 2)
+        y_mean (/ (q/height) 2)
+        x (rg 30 x_mean)
+        y (rg 30 y_mean)]
+    (q/stroke (rg 100 (+ x y)) 30 50 30)
+    (q/point x y)))
 
 (q/defsketch gaussian-distribution
-             :size [200 100]
+             :size [200 200]
              :setup setup
              :draw draw
              :update update
