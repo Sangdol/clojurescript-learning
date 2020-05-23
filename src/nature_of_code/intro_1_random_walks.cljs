68d74eb0b89bcc2c9a112b5eb56a1598adee7cfb
Sanghyun Lee <hammerha@gmail.com>
2020-05-23 22:58:30 +0200

    Add Levy flight

--- src/nature_of_code/intro_1_random_walks.cljs
+++ src/nature_of_code/intro_1_random_walks.cljs
@@ -6,26 +6,44 @@
   (:require [quil.core :as q :include-macros true]
             [quil.middleware :as m]))
 
+(defn rand-update [rand-func]
+  (fn [n]
+    (+ n (rand-func))))
+
+(defn basic-walks []
+  (- (rand-int 3) 1))
+
+(defn levy-flight-walks []
+  (if (< (rand) 0.1)
+    (- (rand-int 200) 100)
+    (basic-walks)))
+
 (defn setup []
   (q/background 240)
   (q/frame-rate 5)
   {:x (/ (q/width) 2) :y (/ (q/height) 2)})
 
-(defn update [state]
-  (letfn [(rand-update [n]
-            (+ n (- (rand-int 3) 1)))]
+(defn update [rand-func]
+  (fn [state]
     (-> state
-      (update-in [:x] rand-update)
-      (update-in [:y] rand-update))))
+        (update-in [:x] (rand-update rand-func))
+        (update-in [:y] (rand-update rand-func)))))
 
 (defn draw [state]
   (q/stroke 0)
   (q/point (:x state) (:y state)))
 
 
-(q/defsketch random-walks
+(q/defsketch random-walks-container
              :size [100 100]
              :setup setup
              :draw draw
-             :update update
+             :update (update basic-walks)
+             :middleware [m/fun-mode])
+
+(q/defsketch levy-flight-container
+             :size [100 100]
+             :setup setup
+             :draw draw
+             :update (update levy-flight-walks)
              :middleware [m/fun-mode])
